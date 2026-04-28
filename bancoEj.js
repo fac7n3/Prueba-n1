7d17cc3da99a7b9164bd3c86f2774e58cdea1707
fac7n3 <shueywater@gmail.com>
2026-04-27 21:48:13 -0300

    se corrigio un error en nombre

--- bancoEj.js
+++ bancoEj.js
@@ -9,16 +9,23 @@ const edadUsuario = Number(prompt("Ingrese su edad:"));
 if (edadUsuario >= 18 && edadUsuario <= 100)
 {
     const nombreUsuario = prompt("Ingrese su nombre:");
-    const apellidoUsuario = prompt("Ingrese su apellido:");
-    const emailUsuario = prompt("Ingrese su email:");
-    const dniUsuario = prompt("Ingrese su DNI:");
-
-    if (dniUsuario.length !== 10) {
-        alert("El DNI debe tener 10 caracteres. Por favor, ingrese un DNI válido.");
+    
+    if (nombreUsuario.length < 3) {
+        alert("El nombre debe tener al menos 3 caracteres. Por favor, ingrese un nombre válido.");
     } else {
-        alert("Bienvenido al banco, " + nombreUsuario + "! Tu edad es: " + edadUsuario);
+        const apellidoUsuario = prompt("Ingrese su apellido:");
+        const emailUsuario = prompt("Ingrese su email:");
+        const dniUsuario = prompt("Ingrese su DNI:");
+
+        if (dniUsuario.length !== 10) {
+            alert("El DNI debe tener 10 caracteres. Por favor, ingrese un DNI válido.");
+        } else {
+            alert("Bienvenido al banco, " + nombreUsuario + "! Tu edad es: " + edadUsuario);
+        }
     }
+
 }
+
  else {
         alert("Lo siento, no puedes hacer el ingreso al banco porque eres menor de edad.");
 
