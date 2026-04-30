43a86103523be03606966cd0716f5a71909c77e1
fac7n3 <shueywater@gmail.com>
2026-04-29 21:27:35 -0300

    se termino la pagina del menu hamb

--- menuhamb.js
+++ menuhamb.js
@@ -1,1 +1,8 @@
-const menudiv = document.getElementById("menudiv");
+const menudiv = document.getElementById("menudiv")
+const menuhamb = document.getElementById("menuhamb")
+menuhamb.addEventListener("mouseenter", () => {
+    menudiv.classList.remove("hidden")
+})
+menuhamb.addEventListener("mouseleave", () => {
+    menudiv.classList.add("hidden")
+})
